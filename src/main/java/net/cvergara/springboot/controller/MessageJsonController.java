0050987138f66c83874cc8de54282caec16dce6b
Cristopher Vergara <c.vergara@nlnexus.com>
2023-07-23 04:11:00 -0400

    JsonConsummer for RabbitMq

--- src/main/java/net/cvergara/springboot/controller/MessageJsonController.java
+++ src/main/java/net/cvergara/springboot/controller/MessageJsonController.java
@@ -1,6 +1,11 @@
 package net.cvergara.springboot.controller;
 
 
+import net.cvergara.springboot.dto.User;
+import net.cvergara.springboot.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class MessageJsonController {
 
-    
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json Message sent to RabbitMQ....");
+
+    }
+
 }
